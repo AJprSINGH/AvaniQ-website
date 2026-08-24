767f9ec75d42f982f3f61b4ad6f34737373eb639
AJprSINGH <ajit.12008255@lpu.in>
2026-08-24 17:14:02 +0530

    resolved header

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,17 @@ import Arrow from "./Arrow";
 
 export default function Header() {
   return (
-    <header className="nav">
-      <a className="logo" href="#home" aria-label="Avaniq Softwares home">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          src="/logo.png"
-          alt="Avaniq Softwares"
-          className="logo-img"
-          width="1280"
-          height="427"
-        />
-      </a>
-      <nav>
-        <a href="#services">Services</a>
-        <a href="#technology">Technology</a>
-        <a href="#work">Projects</a>
-        <a href="#founders">Founders</a>
-        <a href="#contact" className="nav-cta">
-          Start a project <b><Arrow /></b>
+    <>
+      <header className="nav">
+        <a className="logo" href="#home" aria-label="Avaniq Softwares home">
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/logo.png"
+            alt="Avaniq Softwares"
+            className="logo-img"
+            width="1280"
+            height="427"
+          />
         </a>
         <nav>
           <a href="#services">Services</a>
@@ -27,7 +20,7 @@ export default function Header() {
           <a href="#work">Projects</a>
           <a href="#founders">Founders</a>
           <a href="#contact" className="nav-cta">
-            Start a project <b>↗</b>
+            Start a project <b><Arrow /></b>
           </a>
         </nav>
         <button className="menu" aria-label="Toggle menu" aria-expanded="false">
@@ -39,7 +32,7 @@ export default function Header() {
       <div className="mobile-nav-backdrop" aria-hidden="true"></div>
       <aside className="mobile-nav" aria-label="Mobile navigation">
         <div className="mobile-nav-head">
-          <span>AVQ / MENU</span> 
+          <span>AVQ / MENU</span>
         </div>
         <nav>
           <a href="#services">
@@ -55,7 +48,7 @@ export default function Header() {
             <small>04</small>Founders
           </a>
           <a href="#contact" className="mobile-nav-cta">
-            Start a project <b>↗</b>
+            Start a project <Arrow />
           </a>
         </nav>
         <div className="mobile-nav-foot">
